49b3002efff28a62e5651b6300d788b5bf09eef5
Ryzhiy <egor63011@yandex.ru>
2022-03-11 21:48:51 +0500

    2.1 комит mesto после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -5,30 +5,27 @@ const profileEditButton = document.querySelector ('.profile__edit-button');
 /*-----------Объявляю переменные попапа-------*/
 const popup = document.querySelector ('.popup');
 const popupContainer = document.querySelector ('.popup__form');
-const popupInputname = document.querySelector ('.popup__input_name');
-const popupInputdiscription = document.querySelector ('.popup__input_discription');
+const popupInputName = document.querySelector ('.popup__input_name');
+const popupInputDiscription = document.querySelector ('.popup__input_discription');
 const popupClose = document.querySelector ('.popup__close');
-const popupSave = document.querySelector ('.popup__save');
 /*-----------Объявляю функции-------*/
 function popupList () {
-    popup.classList.add('popup__open');
-    popupInputname.value = profileTitle.innerText;
-    popupInputdiscription.value = profileSubtitle.innerText;
+    popup.classList.add('popup_open');  
+    popupInputName.value = profileTitle.textContent;
+    popupInputDiscription.value = profileSubtitle.textContent;
 }
 profileEditButton.addEventListener('click', popupList);
 
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    profileTitle.innerText = popupInputname.value;
-    profileSubtitle.innerText = popupInputdiscription.value;
+    profileTitle.textContent = popupInputName.value;
+    profileSubtitle.textContent = popupInputDiscription.value;
     popupCloseList ();
 }
 
-popupSave.addEventListener('click', formSubmitHandler);
-popupClose.addEventListener('click', popupCloseList);
-
-
 function popupCloseList () {
-    popup.classList.remove('popup__open');
+    popup.classList.remove('popup_open');
     }
+
+    popupClose.addEventListener('click', popupCloseList);
